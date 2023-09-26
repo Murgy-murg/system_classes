82233390fb3f38b66b855dc1b24b1c87a3d48a63
Sam Murgatroyd <sammurgatroyd30@gmail.com>
2023-09-26 10:47:38 +0100

    updated class system commmit

--- lib/todo_list_class_system.py
+++ lib/todo_list_class_system.py
@@ -1,9 +1,9 @@
 class TodoList:
     def __init__(self):
         self.list = []
+        self.complete_list = []
 
     def add(self, todo):
-
         # Parameters:
         #   todo: an instance of Todo
         # Returns:
@@ -13,22 +13,30 @@ class TodoList:
         self.list.append(todo)
         
     
-    
     def incomplete(self):
         # Returns:
         #   A list of Todo instances representing the todos that are not complete
         return self.list
 
     def complete(self):
+
+
+        for todo in self.list:
+            if todo.complete == True:
+                self.complete_list.append(todo.task)
+            
+        return self.complete_list
+            
+
         # Returns:
         #   A list of Todo instances representing the todos that are complete
-        pass
+    
 
     def give_up(self):
         # Returns:
         #   Nothing
         # Side-effects:
         #   Marks all todos as complete
-        pass
-todo_list = TodoList()
-print(todo_list)
+        for todo in self.list:
+            todo.complete = True
+
